1616f228b6d5839259e41886d40cf0d6711dc9fd
wanghuifeng123 <11079921+wanghuifeng123@user.noreply.gitee.com>
2022-07-16 11:11:01 +0800

    更新了文件夹操作

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由
 import router from './router'
+// 引入vuex
 import store from './store'
-
+// 引入全局css
+import './styles/index.css'
 Vue.config.productionTip = false
 
 new Vue({
